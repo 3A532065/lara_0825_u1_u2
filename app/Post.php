7bdd8736baa0791f74a4807772b3c053f0d0e80d
3A532065 <weyna1649@gmail.com>
2019-01-19 12:34:55 +0800

    練習 1~2.4:使用 create方法 設定 Post.php的 fillable屬性

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table='posts';
+    protected $fillable = [
+        'title',
+        'content',
+        'is_feature'
+    ];
+
 }
